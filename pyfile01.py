0d21de7982859ce45bc5f0817eef01925e06628c
Thulo <99490394+thuuthecracker@users.noreply.github.com>
2024-01-30 10:36:43 +0200

    Add files via upload

---
+++ pyfile01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 28 20:27:47 2024
+
+@author: user
+"""
+print('My first Pyhton file')
+#This is my first comment
---
+++ untitled3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 28 20:41:14 2024
+
+@author: user
+"""
+import pandas 
+file = pandas.read_csv("country_data.csv")
+print(file)
+print(file.info())
+print(df.describe())
+
+
+
+
